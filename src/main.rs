72daa9e9e70d8c94826656056fffe4f9ca1918f5
chrysn <chrysn@fsfe.org>
2022-09-18 19:30:19 +0200

    Add documentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+//! CoAP/ACE PoC: Firmware
+//! ======================
+//!
+//! For general introductions, see the project's README.md file. This text is focused at people who
+//! want to not just use the crate, but build on it, understand its workings or alter it.
+//!
+//! Building and running
+//! --------------------
+//!
+//! When developing with this application, it is recommended to use a debugger rather than flashing
+//! a monolithic hex file.
+//!
+//! You'll need:
+//!
+//! * a nightly Rust compiler
+//! * a copy of the [S132 softdevice] (eg. `s132_nrf52_7.3.0_softdevice.hex`)
+//!
+//!   Note that that software is limited in how it can be distributed; you will find the precise
+//!   license terms along with the file.
+//!
+//! * `probe-run`, `probe-rs` and `nrf-recover` installed: `$ cargo install probe-run probe-rs-cli
+//!   nrf-recover`
+//!
+//!   Other debuggers will do as well, but to display debug output, some tool that supports `defmt`
+//!   is needed; `probe-run` does that.
+//!
+//!   <!-- TBD: probably one of them will do -->
+//!
+//! To build and install the firmware:
+//!
+//! * Flash the softdevice. This is only necessary if that version was not installed previously
+//!   already.
+//!
+//!   ```shell
+//!   $ probe-rs-cli download --chip nrf52832_xxAA --format hex /tmp/s132_nrf52_7.3.0/s132_nrf52_7.3.0_softdevice.hex
+//!   ```
+//!
+//!   * If you encounter errors in the style of "Error: AP ApAddress { dp: Default, ap: 0 } is not
+//!     a memory AP", the target chip may be in a locked state; this depends on the previously
+//!     flashed firmware. Run `nrf-recover` to unlock it; this erases all data on the target
+//!     device.
+//!
+//! * Run
+//!
+//!   ```shell
+//!   $ cargo +nightly run
+//!   ```
+//!
+//!   which downloads all relevant crates, builds them and flashes them, all using `probe-run`.
+//!
+//!   After a long horizontal line, the program will print any debug output the firmware produces.
+//!   To increase verbosity, prefix the command with `DEFMT_LOG=info`.
+//!
+//! Once the firmware is flashed, it will start whenever the device is powered.
+//!
+//! [S132 softdevice]: https://www.nordicsemi.com/Products/Development-software/s132/
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
@@ -15,7 +71,10 @@ use nrf_softdevice::{raw, Softdevice};
 
 static EXECUTOR: Forever<Executor> = Forever::new();
 
-/// Background task hand
+/// Background task in which the Softdevice handless all its tasks.
+///
+/// Note that many softdevice tasks are handled in interrupts, which must not be disabled; see the
+/// [nrf_softdevice] documentation for details.
 #[embassy_executor::task]
 async fn softdevice_task(sd: &'static Softdevice) {
     sd.run().await;
@@ -57,6 +116,10 @@ async fn bluetooth_task(sd: &'static Softdevice, server: Server) {
     }
 }
 
+/// Entry function
+///
+/// This assembles the configuration, starts up the softdevice, and lets both the softdevice and a
+/// task for Bluetooth event handling run in parallel.
 #[entry]
 fn main() -> ! {
     info!("Device is starting up...");
