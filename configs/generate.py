2bac3eef53b1648ceb3de6ec60d93ad3effb6ea1
chrysn <chrysn@fsfe.org>
2023-01-11 11:29:45 +0100

    Add documentation for regenerating keys

--- configs/generate.py
+++ configs/generate.py
@@ -1,3 +1,14 @@
+"""
+Script that generated the .yaml files in this directory
+
+This is not run automatically because it would generate different keys each
+time, which would impede the operation of a demo. Outside of a demo context,
+these keys would be generated at commissioning time, and stored in the AS
+(which may also generate them).
+
+When regenerating the keys or adding any, make sure to update the copy of the
+keys that the AS is using.
+"""
 import secrets
 import yaml
 
